2c3ef197929963926f16f2dfb499603d95a4980a
Irenio Reis <79483223+irenio15@users.noreply.github.com>
2021-11-15 21:32:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//cameCase
+//PascalCase
+//snake_case
+
+const LinksSocialMedia = {
+  github: 'irenio15',
+  youtube: 'maykbrito',
+  instagram: 'irenioreis',
+  twitter: 'irenioreis'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https:/${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLink.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
